aae2e82782c63bc35c774ca4f0d2b6305b9d7433
Sladun <sladunarsenov@gmail.com>
2023-08-13 15:41:37 +0300

    adding broadcasting

--- blockchain/src/include/connect.h
+++ blockchain/src/include/connect.h
@@ -3,10 +3,16 @@
 
 #include "util.h"
 
+#define IP_ADDRESS_LOCALHOST "localhost"
+#define IP_ADDRESS_IPv4 "192.168.1.6"
+#define DEFAULT_PORT "27015"
+#define BROADCAST_PORT 12345
+#define RESPONSE_TIMEOUT 5 // Response timeout in seconds
+#define MAX_TIMEOUT (5U)
+
+#define BROADCAST_MESSAGE "Hello"
 #define DEFAULT_BUFLEN 16
 #define ANSWER_LENGHT 1
-
-#define DEFAULT_PORT "27015"
 
 typedef enum
 {
@@ -15,7 +21,6 @@ typedef enum
     TYPE_DOUBLE, /** val=3 **/
 
 } DataType;
-
 
 typedef struct
 {
--- blockchain/src/main.c
+++ blockchain/src/main.c
@@ -18,8 +18,10 @@ block_t block = {0};
 conn_cfg_t iCfg;
 HANDLE  thread_calc,
         thread_recv,
+        thread_bcast,
         mutex_recv;
 transaction_t local_transactions[MAX_TRANSACTIONS_SIZE];
+volatile int stop_listening = 0;
 
 /* Make a function pointer so that it's more flexible.
  * It will be equal to the recv function*/
@@ -56,6 +58,10 @@ iResult_thread WINAPI main_recv(void *);
  **/
 iResult_thread WINAPI main_mine(void *);
 
+iResult_thread WINAPI main_bcast(void *);
+
+iResult start_upd_broadcast_listener(Blockchain *blockchain);
+
 /* Main de-initializing function.
  *  - Closes the sockets.
  *  - Prints the blockchain
@@ -68,13 +74,25 @@ iResult main_close(void);
 int main(int argc, char* argv[]) {
 
     iResult iRes_main = RET_CODE_ERROR;
+
+    iRes_main = send_broadcast_message(&iBlockchain);
+    if (iRes_main == RET_CODE_TIMEOUT) {
+        printf("First member, generating blockchain\n");
+        if((iRes_main = main_open()) != RET_CODE_SUCCESS) {
+            goto MAIN_END;
+        }
+    } else if (iRes_main == RET_CODE_ERROR) {
+        goto MAIN_END;
+    }
+
+    thread_bcast = CreateThread(NULL, 0, main_bcast, NULL, 0, NULL);
+    if (thread_bcast == NULL || thread_bcast == INVALID_HANDLE_VALUE) {
+        printf("CreateThread error: %d\n", GetLastError());
+        return 1;
+    }
     // Check what is done by other blockchains
     // TODO: if first generate, else wait to recieve BC
     /******SETUP*******/
-
-    if((iRes_main = main_open()) != RET_CODE_SUCCESS) {
-        goto MAIN_END;
-    }
 
     // print_blockchain(iBlockchain);
 
@@ -84,12 +102,6 @@ int main(int argc, char* argv[]) {
         printf("CreateMutex error: %d\n", GetLastError());
         return 1;
     }
-
-    /* TODO: Make it so that it fills transactions locally, until a block is "solved/generated"
-     * Then it puts the transaction array inside the block and then it generates hash of the block
-     * based on the transactions or sth.
-     * Then add the block to the blockchain and repeat the cycle.
-     */
 
     /******Begin processes, each rotation is one block added*******/
     uint32_t rotations_BLK = ROTATIONS_BLK;
@@ -209,11 +221,17 @@ iResult_thread main_mine(void *)
 
     build_and_verify_block(&iBlockchain);
     iRes = mine_block(&iBlockchain.blocks[iBlockchain.num_blocks]);
-    
+
     printf("Done mining operation\n");
     return iRes;
 } /* main_mine() */
 #endif
+
+iResult_thread WINAPI main_bcast(void *)
+{
+    iResult_thread iRes = start_upd_broadcast_listener(&iBlockchain);
+    return iRes;
+}
 
 iResult main_close(void)
 {
@@ -224,8 +242,11 @@ iResult main_close(void)
     write_to_file(iBlockchain);
 #endif
 
+    stop_listening = 1;
+    WaitForSingleObject(thread_bcast, MAX_TIMEOUT);
     CloseHandle(thread_calc);
     CloseHandle(thread_recv);
+    CloseHandle(thread_bcast);
     CloseHandle(mutex_recv);
 
 #ifdef USE_CONNECTION
@@ -236,3 +257,71 @@ iResult main_close(void)
     return iResult;
 } /* main_close() */
 /*=====================================================================*/
+
+iResult start_upd_broadcast_listener(Blockchain *blockchain)
+{
+    iResult iRes = RET_CODE_SUCCESS;
+    WSADATA wsaData = {0};
+
+    iRes = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iRes != RET_CODE_SUCCESS) {
+        fprintf(stderr, "WSAStartup failed\n");
+        return iRes;
+    }
+
+    SOCKET sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd == INVALID_SOCKET) {
+        fprintf(stderr, "socket failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        iRes = RET_CODE_ERROR;
+        return iRes;
+    }
+
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(BROADCAST_PORT);
+
+    iRes = bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    if (iRes == SOCKET_ERROR) {
+        fprintf(stderr, "bind failed with error: %ld\n", WSAGetLastError());
+        closesocket(sockfd);
+        WSACleanup();
+        return iRes;
+    }
+
+    char buffer[1024];
+    struct sockaddr_in clientAddr;
+    int clientAddrLen = sizeof(clientAddr);
+
+    while (!stop_listening) {
+        int numBytes = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&clientAddr, &clientAddrLen);
+        if (numBytes == SOCKET_ERROR) {
+            fprintf(stderr, "recvfrom failed with error: %ld\n", WSAGetLastError());
+            closesocket(sockfd);
+            WSACleanup();
+            iRes = RET_CODE_ERROR;
+            return iRes;
+        }
+
+        buffer[numBytes] = '\0';
+        printf("Received message from %s:%d: %s\n", inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port), buffer);
+
+        // Send a response back
+        // Serialize the structure before sending
+        char serializedData[sizeof(Blockchain)];
+        memcpy(serializedData, blockchain, sizeof(Blockchain));
+        printf("Sending blockchain.\n");
+        if (sendto(sockfd, serializedData, strlen(serializedData), 0, (struct sockaddr *)&clientAddr, sizeof(clientAddr)) == SOCKET_ERROR) {
+            fprintf(stderr, "sendto failed with error: %ld\n", WSAGetLastError());
+            closesocket(sockfd);
+            WSACleanup();
+            return 1;
+        }
+    }
+    printf("Got Here\n");
+    closesocket(sockfd);
+    WSACleanup();
+    return 0;
+}
